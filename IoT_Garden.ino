f143fe38ae67364db4a9fe42963fe0ac7e3470a8
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-24 19:56:06 +0800

    set as server

--- IoT_Garden.ino
+++ IoT_Garden.ino
@@ -1,19 +1,107 @@
-int myPin = 5;
-
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "Azhari Home";
+const char* password = "papamama";
+ 
+int ledPin = 5; // GPIO5
+WiFiServer server(80);
+ 
 void setup() {
-  // initialize digital pin 13 as an output.
   Serial.begin(115200);
-  delay(100);
-  pinMode(myPin, OUTPUT);
-  digitalWrite(myPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(10);
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
 }
-
-// the loop function runs over and over again forever
+ 
 void loop() {
-  digitalWrite(myPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.print("On");
-  delay(1000);              // wait for a second
-  digitalWrite(myPin, LOW);    // turn the LED off by making the voltage LOW
-  Serial.print("Off");
-  delay(1000);              // wait for a second
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value = LOW;
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  }
+  if (request.indexOf("/LED=OFF") != -1)  {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+ 
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  client.print("Led pin is now: ");
+ 
+  if(value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");  
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+ 
 }
